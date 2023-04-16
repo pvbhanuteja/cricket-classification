db78169a65cdf7ef1d714f1ed3d8ed7c509e9125
Bhanu <pvbhanuteja@gmail.com>
2023-04-16 02:19:19 -0500

    fix plot

--- main.py
+++ main.py
@@ -26,7 +26,6 @@ class CricketClassifier(LightningModule):
         self.criterion = nn.CrossEntropyLoss()
         self.training_step_outputs = []
         self.val_step_outputs = []
-        self.logger.add_text("id2label", json.dumps(id2label), 0)
 
     def forward(self, x):
         return self.model(x)
@@ -162,7 +161,7 @@ classifier = CricketClassifier(label2id, id2label, num_classes)
 lr_monitor = LearningRateMonitor(logging_interval="step")
 # Set up TensorBoard logger
 tb_logger = TensorBoardLogger("lightning_logs", name="cricket_experiment")
-
+tb_logger.add_text("id2label", json.dumps(id2label), 0)
 # comet_logger = CometLogger(api_key=os.environ.get("COMET_API_KEY"),save_dir="lightning_logs_comet", project_name="cricket_experiment")
 # Initialize the Trainer
 trainer = Trainer(
